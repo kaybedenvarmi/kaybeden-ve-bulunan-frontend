cb78ce14fe24339bab3a32c30af33c0644c8418a
kaybedenvarmi <kaybedenvarmi@gmail.com>
2026-04-20 23:29:20 +0300

    Add files via upload

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "9c49a19405e3d240c0f02b86a1a6c1c4",
+"flutter_bootstrap.js": "fac784f471f1aed6f318ee386d4c50f4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
